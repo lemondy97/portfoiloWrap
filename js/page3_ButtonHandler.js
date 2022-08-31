bd7c5766120f5f3ab78cb70399ca9c485e9a53bc
lemondy97 <104114377+lemondy97@users.noreply.github.com>
2022-08-31 13:53:42 +0900

    20220831_folio_jingyu1
    포트폴리오 묶음 업데이트

--- js/page3_ButtonHandler.js
+++ js/page3_ButtonHandler.js
@@ -1,53 +1,22 @@
-	  const opt = {
-              scrollGauge: 0,
-              wrapClass: document.querySelector('div#page3Wrap'),
-              section: document.getElementsByClassName('portfolioImage'),
-              sectionHeight() {
-                  return this.section[0].clientHeight
-              },
-              maxValue() {
-                  return (this.section.length - 1) * this.sectionHeight()
-              },
-              currentSection() {
-                  return parseInt(opt.scrollGauge / opt.sectionHeight())
-              },
-              limitValidate() {
-                  if (this.scrollGauge < 0) this.scrollGauge = 0;
-                  else if (this.scrollGauge > this.maxValue()) this.scrollGauge = this.maxValue();
-              },
-              applyHeightValue(elem, idx, value) {
-                  elem.style.bottom = -(this.sectionHeight() * idx) + value + 'px'
-              },
-              initialTopValue(value) {
-                  [...this.section].forEach((elem, idx) => this.applyHeightValue(elem, idx, value))
-              },
-              scrollValue(value) {
-                  return this.sectionHeight() * value
-              },
-              controlSections(value) {
-                  this.initialTopValue(this.scrollValue(value));
-              },
-              changeSection(val) {
-                  return (opt.currentSection() - val) * opt.sectionHeight();
-              },
-              moveSectionWithinLimit() {
-                  this.limitValidate();
-                  this.controlSections(this.currentSection());
-              }
-          }
-          let btnHandler;
-          btnHandler = function (e) {
-              const target = e.target;
-              if (!target.classList.contains('UpDown')) return;
-              switch (target.textContent.toLowerCase()) {
-                  case 'up':
-                      opt.scrollGauge = opt.changeSection(1);
-                      break;
-                  case 'down':
-                      opt.scrollGauge = opt.changeSection(-1);
-                      break;
-              }
-              opt.moveSectionWithinLimit();
-          }
-          opt.initialTopValue(opt.currentSection());
-          window.addEventListener('click', btnHandler);
+$(function(){
+
+
+    var i = 0;
+    var t = 0;
+    $('.UpDownBox').click(function(){
+      i++;
+      t++;
+      if(i > $('.movingBox').length-1){
+        i = 0;
+      }
+      if(t > $('.hiddenText').length-1){
+        t = 0;
+      }
+      $('.movingBox').eq(i).stop().fadeIn();
+      $('.movingBox').eq(i-1).stop().fadeOut();
+
+      $('.hiddenText').eq(t).stop().fadeIn(); 
+      $('.hiddenText').eq(t-1).stop().fadeOut();        
+    });
+    
+  });
